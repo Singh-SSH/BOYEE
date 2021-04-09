233b9338cb2f2a5214dc5472dc7ce2644827e782
Singh-SSH <73661000+Singh-SSH@users.noreply.github.com>
2021-04-09 18:20:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,39 @@
 function preload() {
 }
 
-
 function setup() {
-  createCanvas(500, 500);
+  canvas = createCanvas(640, 480);
+  canvas.position(150, 150);
   video = createCapture(VIDEO);
+  video.hide();
+
 }
-webcam.set()
-function draw(){
-    rect(60, 60, 300, 25,);
-    rect(40, 60, 25, 300,);
-    rect(335, 60, 25, 300,);
-    rect(60, 340, 300, 25,);
-    circle(50,70,60 );
-    circle(350,70,60 );
-    circle(50,350,60 );
-    circle(350,350,60 );
+
+function draw() {
+  image(video, 230, 150, 220, 200);
+
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  circle(50, 50, 80);
+
+  circle(50, 430, 80);
+
+  circle(590, 50, 80);
+
+  circle(590, 430, 80);
+
+  fill(0, 128, 0);
+  stroke(0, 128, 0);
+  rect(90, 40, 460, 20);
+
+  rect(90, 420, 460, 20);
+
+  rect(40, 90, 20, 300);
+
+  rect(580, 90, 20, 300);
 }
+
 function take_snapshot(){    
   save('student_name.png');
 }
+
